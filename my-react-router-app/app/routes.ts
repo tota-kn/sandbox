c2b4331e95f7f8fa142dd5dafa51853788741252
tota-kn <nonbiriactive@gmail.com>
2025-03-22 18:32:34 +0900

    React Routerのリンクを修正し、Outletを追加

--- my-react-router-app/app/routes.ts
+++ my-react-router-app/app/routes.ts
@@ -1,5 +1,5 @@
 import { route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
-    route("contacts/:contactId", "routes/contacts.tsx")
+    route("contacts/:contactId", "routes/contact.tsx")
 ] satisfies RouteConfig;
