f7feb57f160021f90b800b11538607f99a212123
tota-kn <nonbiriactive@gmail.com>
2025-03-30 17:44:50 +0900

    update

--- extention-bookmark-tag/components/TagBadge.vue
+++ extention-bookmark-tag/components/TagBadge.vue
@@ -2,8 +2,29 @@
   <span 
     @click="$emit('toggle')"
     :class="[
-      ...commonClasses,
-      ...(props.selected ? selectedClasses : unselectedClasses)
+      // 共通スタイル
+      'px-3',
+      'py-1',
+      'rounded-full',
+      'text-sm',
+      'cursor-pointer',
+      'transition-all',
+      'duration-200',
+      'inline-block',
+      
+      // 選択状態に応じたスタイル
+      props.selected ? [
+        'bg-blue-500',
+        'text-white',
+        'border',
+        'border-blue-600'
+      ] : [
+        'bg-blue-100',
+        'text-blue-700',
+        'border',
+        'border-blue-200',
+        'hover:bg-blue-200'
+      ]
     ]"
   >
     {{ props.tag }}
@@ -20,33 +41,4 @@ const props = defineProps<{
 defineEmits<{
   (e: 'toggle'): void;
 }>();
-
-// 共通スタイルクラス
-const commonClasses = [
-  'px-3',
-  'py-1',
-  'rounded-full',
-  'text-sm',
-  'cursor-pointer',
-  'transition-all',
-  'duration-200',
-  'inline-block'
-];
-
-// 選択時のスタイルクラス
-const selectedClasses = [
-  'bg-blue-500',
-  'text-white',
-  'border',
-  'border-blue-600'
-];
-
-// 非選択時のスタイルクラス
-const unselectedClasses = [
-  'bg-blue-100',
-  'text-blue-700',
-  'border',
-  'border-blue-200',
-  'hover:bg-blue-200'
-];
 </script>
