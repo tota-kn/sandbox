5bb868c28a47899ec609ec97fb66f34cca4d4652
tota-kn <nonbiriactive@gmail.com>
2025-04-01 23:28:26 +0900

    update

--- extention-bookmark-tag/components/SearchBox.vue
+++ extention-bookmark-tag/components/SearchBox.vue
@@ -8,45 +8,21 @@
       @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
     >
     <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 text-gray-400"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-        />
-      </svg>
+      <MagnifyingGlassIcon class="size-4  text-gray-400" />
     </div>
     <button 
       v-if="modelValue"
       class="absolute inset-y-0 right-0 pr-3 flex items-center" 
       @click="$emit('update:modelValue', '')"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 text-gray-400 hover:text-gray-600"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M6 18L18 6M6 6l12 12"
-        />
-      </svg>
+      <XMarkIcon class="size-4 text-gray-400 hover:text-gray-600" />
     </button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { MagnifyingGlassIcon, XMarkIcon } from '@heroicons/vue/24/outline';
+
 /**
  * SearchBoxコンポーネントのプロパティ定義
  */
